019c7a1df1d205f7544079bab416e4d501ab4bff
Sergio <maserasen@gmail.com>
2018-12-10 02:57:11 +0100

    Portada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortadaComponent } from 'src/app/components/portada/portada.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PortadaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
