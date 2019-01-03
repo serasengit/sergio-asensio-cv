22de8de0b1c0a95debebdf57e7c93b04d61478c1
Sergio <maserasen@gmail.com>
2019-01-03 03:58:48 +0100

    Server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-//Install express server
+// Install express server
 const express = require('express');
 const path = require('path');
 
@@ -7,9 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/sergio-asensio-cv'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/sergio-asensio-cv/index.html'));
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname, '/dist/sergio-asensio-cv/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
