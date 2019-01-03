959b2ed1f03e86311ee68065d999ebf690d2dc2f
Sergio <maserasen@gmail.com>
2019-01-03 05:18:57 +0100

    Server Configuration

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 // Install express server
+console.log('b');
 const express = require('express');
 const path = require('path');
 
@@ -12,5 +13,24 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/sergio-asensio-cv/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// Allow any method from any host and log requests
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+    if ('OPTIONS' === req.method) {
+        res.sendStatus(200);
+    } else {
+        console.log(`${req.ip} ${req.method} ${req.url}`);
+        next();
+    }
+});
+// Handle POST requests that come in formatted as JSON
+app.use(express.json());
+// A default hello word route
+app.get('/hello', (req, res) => {
+    res.send({ hello: 'world' });
+});
+
+// Start the app by listening on the default Heroku port or locally
+app.listen(process.env.PORT || 4201);
