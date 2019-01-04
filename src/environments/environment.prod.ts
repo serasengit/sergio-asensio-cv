9011c694210830430b228185b1f11b2b8ac5daf2
Sergio <maserasen@gmail.com>
2019-01-04 03:27:48 +0100

    HTTP Server Errors Handling and Email Sending

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 
 export const environment = {
   production: true,
+  APP_BASEURL: 'http://localhost:4200',
+  APP_NAME: 'sergio-asensio-cv',
+  DATABASE_URL: '',
+  APP_PORT: '4200',
+  SERVER_URL: 'http://localhost:4201',
+  SERVER_PATH: 'api',
+  SERVER_PORT: '4201'
 };
