bcf556d7bfca360cd7a3594610db103a99d09590
Sergio <maserasen@gmail.com>
2019-01-05 15:16:48 +0100

    Git Ignore

--- server/db/knex/migrations/20190105123607_create_config_table.js
+++ server/db/knex/migrations/20190105123607_create_config_table.js
@@ -10,6 +10,6 @@ exports.up = function (knex, Promise) {
 };
 
 exports.down = function (knex, Promise) {
-    return knex.schema.dropTable('T0001_CONFIGs');
+    return knex.schema.dropTable('T0001_CONFIG');
 
 };
