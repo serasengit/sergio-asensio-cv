1d5959c424a8235bab9879df5eef0945a1c92be2
Sergio <maserasen@gmail.com>
2019-02-02 01:16:01 +0100

    Graphs development

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,17 +1,61 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html'
 })
 export class SkillsComponent implements OnInit, AfterViewInit {
-
   constructor(private spinnerService: Ng4LoadingSpinnerService) { }
 
+  private canvas: any;
+  private ctx: any;
+
   ngOnInit() {
+    this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+    const myChart = new Chart(this.ctx, {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 4, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: false,
+        display: true,
+        scales: {
+          yAxes: [{
+            ticks: {
+              max: 20,
+              min: 0,
+            }
+          }]
+        }
+      }
+    });
   }
-  ngAfterViewInit() {
-  }
+  ngAfterViewInit() { }
+
 
 }
