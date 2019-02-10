5fd03bc8909d415e774072b3656f0921cb035b72
Sergio <maserasen@gmail.com>
2019-02-10 05:27:11 +0100

    Skills Sections

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,61 +1,117 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 import { Chart } from 'chart.js';
+import * as $ from 'jquery';
+import { UtilsService } from 'src/utils/utils.service';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-skills',
-  templateUrl: './skills.component.html'
-})
-export class SkillsComponent implements OnInit, AfterViewInit {
-  constructor(private spinnerService: Ng4LoadingSpinnerService) { }
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss']
 
-  private canvas: any;
-  private ctx: any;
+})
+export class SkillsComponent implements OnInit {
+  constructor(private spinnerService: Ng4LoadingSpinnerService, private _UtilsService: UtilsService) { }
+
+  private softWareChart: any;
+  private ctxSoftWareChart: any;
+  private englishChart: any;
+  private ctxEnglishChart: any;
+
+
 
   ngOnInit() {
-    this.canvas = document.getElementById('myChart');
-    this.ctx = this.canvas.getContext('2d');
-    const myChart = new Chart(this.ctx, {
-      type: 'line',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 4, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        responsive: false,
-        display: true,
-        scales: {
-          yAxes: [{
-            ticks: {
-              max: 20,
-              min: 0,
-            }
-          }]
-        }
-      }
-    });
+    this.generateCharts();
   }
-  ngAfterViewInit() { }
+  generateCharts() {
+    this.softWareChart = document.getElementById('softWareChart');
+    this.englishChart = document.getElementById('englishChart');
+
+    if (!isNullOrUndefined(this.softWareChart)) {
+      this.ctxSoftWareChart = this.softWareChart.getContext('2d');
+      const softWareChart = new Chart(this.ctxSoftWareChart, {
+        type: 'bar',
+        data: {
+          labels: ['Africa', 'Asia', 'Europe', 'Latin America', 'North America'],
+          datasets: [
+            {
+              label: '% Knowledge',
+              backgroundColor: ['#3e95cd', '#8e5ea2', '#3cba9f', '#e8c3b9', '#c45850'],
+              data: [2478, 5267, 734, 784, 433]
+            }
+          ]
+        },
+        options: {
+          legend: { display: false },
+          title: {
+            display: true,
+            text: 'Software Skills',
+            padding: 20,
+            fontSize: '32',
+            fontColor: 'black',
+            fontFamily: 'Serif'
+          },
+          maintainAspectRatio: false
+
+        }
+      });
+    }
+    if (!isNullOrUndefined(this.englishChart)) {
+      this.ctxEnglishChart = this.englishChart.getContext('2d');
+      const englishChart = new Chart(this.ctxEnglishChart, {
+        type: 'doughnut',
+        data: {
+          datasets: [
+            {
+              label: 'English',
+              backgroundColor: ['#4169E1', '#87CEFA'],
+              data: [70, 30]
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          tooltips: { enabled: false },
+          title: {
+            display: true,
+            text: 'English',
+            fontSize: '32',
+            fontColor: 'black',
+            padding: 20,
+            fontFamily: 'Serif'
+
+          },
+          maintainAspectRatio: false
+
+        },
+        plugins: [{
+          id: 'englishChart',
+          afterDraw: function (chart, option) {
+            chart.ctx.fillStyle = 'black';
+            chart.ctx.textBaseline = 'middle';
+            chart.ctx.textAlign = 'center';
+            chart.ctx.font = '35px Arial';
+            chart.ctx.fillText('70%', ((chart.chartArea.left + chart.chartArea.right) / 2),
+              ((chart.chartArea.top + chart.chartArea.bottom) / 2));
+          }
+        }]
+      });
+
+    }
+
+  }
+  resetCharts() {
+    $('canvas').remove();
+    $('.item1').prepend('<canvas id="softWareChart" ></canvas>');
+    $('.item2').prepend('<canvas id="englishChart" ></canvas>');
+    setTimeout(() => {
+      this.generateCharts();
+    }, 500);
+
+  }
 
 
 }
